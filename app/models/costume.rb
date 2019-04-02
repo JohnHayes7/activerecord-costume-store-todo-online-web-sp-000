a24a3ccdf1ca836e1acdc331a50d370136408f5b
John Hayes <johnnyhayes7@gmail.com>
2019-04-02 16:55:01 +0000

    Automatically backed up by Learn

--- app/models/costume.rb
+++ app/models/costume.rb
@@ -3,4 +3,12 @@
 
 class Costume < ActiveRecord::Base
   
+  def change
+    create_table :costumes do |c|
+      c.text :name 
+      c.integer :price
+      c.text :size
+      c.text :image_url
+      c.timestamp
+  
 end
