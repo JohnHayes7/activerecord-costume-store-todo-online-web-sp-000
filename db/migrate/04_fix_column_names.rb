9afa65a2e92db652497439f3042c42cb89e11496
John Hayes <johnnyhayes7@gmail.com>
2019-04-02 17:43:30 +0000

    Automatically backed up by Learn

--- db/migrate/04_fix_column_names.rb
+++ db/migrate/04_fix_column_names.rb
@@ -2,5 +2,6 @@ class FixColumnNames < ActiveRecord::Migration[4.2]
   
   def change
     rename_column(:costume_stores, :number_of_employees, :num_of_employees)
+    rename_column(:haunted_houses, :long_description, :description)
   end
 end
