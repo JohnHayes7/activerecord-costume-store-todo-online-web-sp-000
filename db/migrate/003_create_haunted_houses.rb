3ebeb9e8a72e8d8a5ce3bcf7367bcae749e48eb2
John Hayes <johnnyhayes7@gmail.com>
2019-04-02 17:24:05 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -10,5 +10,6 @@ class CreateHauntedHouses < ActiveRecord::Migration[4.2]
       h.integer :price
       h.boolean :family_friendly
       h.datetime :opening_date
+      h.datetime :closing_date
   
 end
