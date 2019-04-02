8047779fd25e3292cefa64180f4741ad1a16e332
John Hayes <johnnyhayes7@gmail.com>
2019-04-02 17:42:58 +0000

    Automatically backed up by Learn

--- db/migrate/04_fix_column_names.rb
+++ db/migrate/04_fix_column_names.rb
@@ -1,6 +1,6 @@
 class FixColumnNames < ActiveRecord::Migration[4.2]
   
   def change
-    rename_column(:costume_stores)
+    rename_column(:costume_stores, :number_of_employees, :num_of_employees)
   end
 end
