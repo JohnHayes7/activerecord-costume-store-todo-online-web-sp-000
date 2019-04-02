0972f6c218e369b79dbab6d488f00a69001cd803
John Hayes <johnnyhayes7@gmail.com>
2019-04-02 17:01:02 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -14,7 +14,9 @@ class CreateCostumes < ActiveRecord::Migration
     create_table :costumes do |c|
       c.text :name
       c.integer :price
-      c.text :size
+      c.text :size 
+      c.text :image_url
+      c.timestamps
       
     end
   end
